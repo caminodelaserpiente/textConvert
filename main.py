48693ebf0d9367a7499b834ebb58d1d277884a54
蛇道 <caminodelaserpiente.py@gmail.com>
2023-08-12 22:34:53 -0600

    [ADD] Launcher Application

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Author: 蛇道
+Url: https://github.com/caminodelaserpiente
+"""
+
+
+from textConvert import textConvert
+
+
+def main():
+    textConvert()
+
+
+if __name__ == "__main__":
+    main()
